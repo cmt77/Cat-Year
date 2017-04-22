3158f168cfd94836a19ac722216f660540306e14
Tim <cmt77@outlook.com>
2017-04-22 20:49:56 +0800

    Initial Commit

---
+++ Cat Year/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Cat Year
+//
+//  Created by Tim Tseng on 2016/12/21.
+//  Copyright © 2016年 mct77. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var ageTextField: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func submitPressed(_ sender: Any) {
+        if ageTextField.text != "" {
+            let resultAge = Int(ageTextField.text!)! * 7
+            resultLabel.text = String(resultAge)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
